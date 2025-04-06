67fac7f349e5b5fd177c747cc66d7309532002c0
Jason Schwarz <jason.c.schwarz@gmail.com>
2025-04-06 12:06:12 -0300

    bug: redeploy to Base Sepolia

--- script/DeployProxy.s.sol
+++ script/DeployProxy.s.sol
@@ -5,11 +5,11 @@ import "forge-std/Script.sol";
 import {ArtStaking} from "contracts/ArtStaking.sol";
 import {Upgrades, Options} from "openzeppelin-foundry-upgrades/Upgrades.sol";
 
-//forge script script/DeployProxy.s.sol:ArtStakingScript --rpc-url $RPC_URL --broadcast -vvvv --via-ir
+// forge script script/DeployProxy.s.sol:ArtStakingScript --rpc-url $RPC_URL --broadcast -vvvv --via-ir
 
 // https://docs.openzeppelin.com/upgrades-plugins/1.x/api-foundry-upgrades#Upgrades-Upgrades-deployTransparentProxy-string-address-bytes-
 
-//https://docs.openzeppelin.com/upgrades-plugins/1.x/api-core#define-reference-contracts
+// https://docs.openzeppelin.com/upgrades-plugins/1.x/api-core#define-reference-contracts
 
 contract ArtStakingScript is Script {
     error TransactionFailed(string message);
@@ -22,7 +22,7 @@ contract ArtStakingScript is Script {
         address initialOwner = derivedAddress;
 
         // ArtStaking initialization parameters
-        address artTokenAddress = 0x3CAddA76d9CC7AE01cA1e228F42e617725120A55; // OFT testnet
+        address artTokenAddress = 0x682F564b7292d9156E779e40B05224A50CdDCA61; // OFT testnet
         uint256 stakingEnabledAt = block.timestamp; // Or set specific timestamp
         uint256 threeMonthRewardMultiplier = 0.2e18; // 20% reward
         uint256 sixMonthRewardMultiplier = 0.5e18; // 50% reward
